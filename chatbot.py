886b99aba4c585a857f4914b7ce4cb02af85d619
33ss348 <sesantos2013@gmail.com>
2025-09-02 15:50:54 +0100

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+print(f"hello I am an AI bot whats your name") 
+name=input()
+print(f"nice to meet you")
+print(f"how are you feeling to day")
+mood=input.lower()
+if mood=="good":
+    print(f"nice to hear that")
+elif mood=="bad":
+    print(f"sorry to hear that")
+else:
+    print(f"please try angian")
